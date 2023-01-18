a2b857286de783a3f9a03a2d62dce7c6fa80e3c2
oayouub <91758300+oayouub@users.noreply.github.com>
2023-01-18 10:52:46 +0100

    base données sql fin

--- dev cours/NodeJS cours/Node-Pokemon-Api/app.js
+++ dev cours/NodeJS cours/Node-Pokemon-Api/app.js
@@ -2,11 +2,30 @@ const express = require('express') //chercher la dependance 'express
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
+const { Sequelize } = require('sequelize')
 const { success, getUniqueId } = require('./helper')
 let pokemons = require('./mock.pokemon');
 
 const app = express() //instance dune application express(serveur web)
 const port = 3000 //port
+
+const sequelize = new Sequelize(
+    'pokedex',
+    'root',
+    '',
+    {
+        host: 'localhost',
+        dialect: 'mariadb',
+        dialectOprions: {
+            timezone: 'Etc/GMT-2'
+        },
+        logging: false
+    }
+)
+
+sequelize.authenticate()
+.then(_=> console.log('La connexion à la base de données a bien été établie.'))
+.catch(error => console.error(`Impossible de se connecter a la base de données ${error}`))
 
 app
 .use(favicon(__dirname + '/favicon.ico'))
@@ -29,7 +48,7 @@ app.get('/api/pokemons',(req,res)=>{
     })
 
 app.post('/api/pokemons', (req, res) => {
-    const id = getUniqueId
+    const id = getUniqueId(pokemons)
     const pokemonCreated = {...req.body, ...{id: id, created: new Date()}}
     pokemons.push(pokemonCreated)
     const message = `Le pokémon ${pokemonCreated.name} a bien été crée.`
@@ -48,7 +67,8 @@ app.put(`/api/pokemons/:id`, (req, res) =>{
 
 app.delete('/api/pokemons/:id', (req, res) =>{
     const id = parseInt(req.params.id)
-    const pokemonDeleted = pokemons.find(pokemon => pokemon.id ===id)
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+    pokemons = 
     pokemons.filter(pokemon => pokemon.id !==id)
     const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé.`
     res.json(success(message, pokemonDeleted))
