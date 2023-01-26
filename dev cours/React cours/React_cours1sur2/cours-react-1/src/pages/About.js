156a3585c77cc654975365c0d8e3cef8dafacb6c
oayouub <91758300+oayouub@users.noreply.github.com>
2023-01-26 08:55:07 +0100

    add react cours

---
+++ dev cours/React cours/React_cours1sur2/cours-react-1/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae sunt qui voluptas aperiam magnam dolores delectus nostrum necessitatibus animi. Adipisci iure doloremque vel quasi, corrupti voluptate repellendus soluta officia molestiae eius praesentium nam iusto aliquid laborum fuga, maxime reiciendis, et beatae. Et quo explicabo voluptatibus a fugiat mollitia sapiente distinctio amet exercitationem temporibus earum laboriosam repudiandae ea delectus placeat error praesentium, eveniet assumenda. Pariatur non deserunt tenetur voluptatem eveniet, quae, illo vero voluptas mollitia reiciendis nisi omnis. Rem ipsam molestias et soluta repellendus totam ad quam! Dignissimos, excepturi ipsa sapiente vitae alias quo, natus magni cum nisi sunt modi vero!</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam corrupti officiis architecto! Eaque quae harum provident labore totam veniam placeat? Nam fuga possimus laborum pariatur laudantium. Iusto fugit laborum omnis illum non reiciendis tempora deserunt iure quas nemo, magni, iste, quis quidem quod in! Quo nostrum dicta molestiae asperiores inventore ex minima dolorum praesentium, impedit enim suscipit quas, aperiam tempore, quos voluptatem odit recusandae nam similique perspiciatis? Deleniti dolorem facilis porro ullam dolore animi adipisci totam ab necessitatibus numquam, nobis obcaecati aspernatur alias tempore, nostrum similique praesentium beatae id corrupti a commodi aliquam voluptatibus, perspiciatis ratione. Eligendi, deleniti odio? Provident.</p>
+
+        </div>
+    );
+};
+
+export default About;
